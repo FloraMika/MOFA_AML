acd8f1db50a57d5e847e90162b637c24f9a32072
FloraMika <flora.mikaeloff@free.fr>
2026-05-27 16:27:12 +0200

    update pipeline

--- src/Run_pipeline.R
+++ src/Run_pipeline.R
@@ -4,14 +4,13 @@
 ## Input files ##
 ###############
 
-load("/home/flomik/Data/MOFA_R_Objects/AML_MAexp_v05.RData")
-load("/home/flomik/Data/MOFA_R_Objects/MOFA_v04_train_list.RData")
-
-# AML_MAexp_v05.RData : MultiAssayExperiment object with filtered omics and DSRT data
+# AML_MAexp_v05.RData : MultiAssayExperiment object with omics and DSRT data
+# pData : dataframe including clinal parameters for retrospective cohort
+# AML_MAexp_v05_filt.RData : MultiAssayExperiment object with filtered and scaled omics and DSRT data
 # MOFA_v04_train_list.RData : MOFA output including MOFA model
 # Wells_combined_sctransformed.rds : Seurat object with CITE-seq data
 # Ensembl2Reactome_All_Levels.txt : GMT file including reactome pathways and associated genes
-# Clinical_mofa_retro.xlsx : dataframe including clinal parameters for retrospective cohort
+
 
 ###############
 ## Run Pipeline ##
@@ -25,40 +24,35 @@ library(rmarkdown)
 
 # Pre-processing of omics and DSRT data (R v3.6.0, MOFA2 v0.99.7)
 
-## Merge cells markers (R v4.4.2)
-render("~/Code/MOFA_AML/src/3_Analysis_local/1_processing/pre_processing_AML_cell_types_markers.Rmd")
-
 ### Run MOFA (R v3.6.0, MOFA2 v0.99.7)
-render("~/Code/MOFA_AML/src/1_MOFA_training/MOFA_v04_train.r")
-render("~/Code/MOFA_AML/src/2_Analysis_server/Downstream_analysis_MOFA.Rmd")
-
-# CITE-seq data based scores 
-render("~/Code/MOFA_AML/src/2_Analysis_server/Clinseq_pro_RNAseq_AML_cell_types_vs_factors.Rmd") #(R v3.6.0, Seurat v3.2.2)
-render("~/Code/MOFA_AML/src/3_Analysis_local/2_validation/CITE_seq_association_scores_cell_types_surface_markers.Rmd") #(R v4.4.2, Seurat v5.3)
+render("~/Code/MOFA_AML/src/1_MOFA_training/1_MOFA_v04_train.Rmd")
+render("~/Code/MOFA_AML/src/1_MOFA_training/2_Downstream_analysis_MOFA.Rmd")
 
 # Transcriptomics based scores and survival analysis (R v4.4.2)
-render("~/Code/MOFA_AML/src/3_Analysis_local/2_validation/hallmarks_scores_based_transcriptomics_survival.Rmd")
+render("~/Code/MOFA_AML/src/2_Analysis_local/1_survival/hallmarks_scores_based_transcriptomics_survival_v2.Rmd")
 
 ### Figures (R v4.4.2)
-render("~/Code/MOFA_AML/src/3_Analysis_local/3_figures/extract_top_drugs_rank_factors_MOFA.Rmd")
-render("~/Code/MOFA_AML/src/3_Analysis_local/3_figures/figures_MOFA_variance_per_factor.Rmd")
-render("~/Code/MOFA_AML/src/3_Analysis_local/3_figures/heatmap_half_triangles_pathways_MOFA.Rmd")
-render("~/Code/MOFA_AML/src/3_Analysis_local/3_figures/sankey_plot_cell_types_MOFA.Rmd")
+render("~/Code/MOFA_AML/src/2_Analysis_local/2_figures/1_figures_MOFA_variance_per_factor.Rmd")
+render("~/Code/MOFA_AML/src/2_Analysis_local/2_figures/2_heatmap_half_triangles_pathways_MOFA.Rmd")
+render("~/Code/MOFA_AML/src/2_Analysis_local/2_figures/3_extract_top_drugs_rank_factors_MOFA.Rmd")
+render("~/Code/MOFA_AML/src/2_Analysis_local/2_figures/4_scatter_plots_drugs_factors.Rmd")
+render("~/Code/MOFA_AML/src/2_Analysis_local/2_figures/hallmark_circularplot_Tojo.r")
+
 
 ### Review (R v4.4.2)
 
 #### extra figures
-render("~/Code/MOFA_AML/src/4_review/1_figures/Tojo/hallmark_circularplot.r")
-render("~/Code/MOFA_AML/src/4_review/1_figures/1_review_figures.Rmd")
+render("~/Code/MOFA_AML/src/3_review/1_figures/1_review_figures.Rmd")
+
 #### Robustness
-render("~/Code/MOFA_AML/src/4_review/2_robustness/1_MOFA_robustness_generate_data_run_models.Rmd")
-render("~/Code/MOFA_AML/src/4_review/2_robustness/2_MOFA_robustness_generate_data_run_models_part_2.Rmd")
-render("~/Code/MOFA_AML/src/4_review/2_robustness/3_MOFA_robustness_correlations_2.Rmd")
+render("~/Code/MOFA_AML/src/3_review/2_robustness/1_MOFA_robustness_generate_data_run_models.Rmd")
+render("~/Code/MOFA_AML/src/3_review/2_robustness/2_MOFA_robustness_generate_data_run_models_part_2.Rmd")
+render("~/Code/MOFA_AML/src/3_review/2_robustness/3_MOFA_robustness_correlations_2.Rmd")
 
 #### validation in single cell
-render("~/Code/MOFA_AML/src/4_review/3_scRNA/1_KI_scRNA_analysis_BMMap_v2.rmd")
-render("~/Code/MOFA_AML/src/4_review/3_scRNA/2_Validation_scRNA_analysis_BMMap_Zheng_v2.rmd")
+render("~/Code/MOFA_AML/src/3_review/3_scRNA/1_KI_scRNA_analysis_BMMap_v2.rmd")
+render("~/Code/MOFA_AML/src/3_review/3_scRNA/2_Validation_scRNA_analysis_BMMap_Zheng_v2.rmd")
 
 #### change sign
-render("~/Code/MOFA_AML/src/4_review/4_sign/1_change_sign_figures_single_cells.rmd")
-render( "~/Code/MOFA_AML/src/4_review/4_sign/1_change_sign_figures_single_cells.rmd")
+render("~/Code/MOFA_AML/src/3_review/4_sign/2_change_sign_figures_beatAML.rmd")
+
